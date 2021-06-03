4b66ae596622e0a2574f721ec06892e7395c6542
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-06-04 00:01:42 +0200

    I think it finishes random numbers

--- Random Numbers/Random Numbers.cpp
+++ Random Numbers/Random Numbers.cpp
@@ -1,20 +1,29 @@
-// Random Numbers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    cout << "Type your desired number of iterations: " << endl;
+    float iterations;
+    cin >> iterations;
+
+    float randomPool[20] = {};
+    int randomNumber;
+
+    srand(time(0));
+
+    for (int i = 0; i <= iterations; i++) {
+        randomNumber = rand() % 20 + 1;
+        randomPool[randomNumber - 1] += 1;
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < 20;i++) {
+        float selectRandom = (randomPool[i] / iterations) * 100;
+        cout << "Number: " <<  i + 1  << " with a probability of: " << randomPool[i] << "(" << selectRandom << " % )" << endl;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
